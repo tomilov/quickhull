828aafcf3490afbecf0cfa97e6bea3abc160c5fa
Tomilov Anatoliy <tomilovanatoliy@gmail.com>
2015-08-10 17:10:05 +0500

    #

--- test/qprof.sh
+++ test/qprof.sh
@@ -3,6 +3,6 @@
 PROJECT_ROOT=$(dirname $0)/..
 
 rm /tmp/qprof.pdf
-rbox n D10 38 s t | PROFILEFREQUENCY=10000 LD_PRELOAD=/usr/lib/libprofiler.so.0 CPUPROFILE=$PROJECT_ROOT/bin/quickhull.profile /tmp/quickhull
-google-pprof --pdf /tmp/quickhull $PROJECT_ROOT/bin/quickhull.profile > /tmp/qprof.pdf
-gnome-open /tmp/qprof.pdf
+rbox n D10 30 s t | PROFILEFREQUENCY=10000 LD_PRELOAD=/usr/lib/libprofiler.so.0 CPUPROFILE=$PROJECT_ROOT/bin/quickhull.profile ../bin/quickhull
+google-pprof --pdf ../bin/quickhull $PROJECT_ROOT/bin/quickhull.profile > ../bin/qprof.pdf
+gnome-open ../bin/qprof.pdf
