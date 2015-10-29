ad06964e711adbeb1f5d153b045450c806d0096c
Tomilov Anatoliy <tomilovanatoliy@gmail.com>
2015-10-29 16:51:35 +0500

    profiling target directory changed to fit cmake-builded binary destination

--- test/qprof.sh
+++ test/qprof.sh
@@ -3,6 +3,6 @@
 PROJECT_ROOT=$(dirname $0)/..
 
 rm /tmp/qprof.pdf
-rbox n D10 30 s t | PROFILEFREQUENCY=10000 LD_PRELOAD=/usr/lib/libprofiler.so.0 CPUPROFILE=$PROJECT_ROOT/bin/quickhull.profile ../bin/quickhull
-google-pprof --pdf ../bin/quickhull $PROJECT_ROOT/bin/quickhull.profile > ../bin/qprof.pdf
-gnome-open ../bin/qprof.pdf
+rbox n D10 35 s t | PROFILEFREQUENCY=10000 LD_PRELOAD=/usr/lib/libprofiler.so.0 CPUPROFILE=$PROJECT_ROOT/build/quickhull.profile ../build/quickhull
+google-pprof --pdf ../build/quickhull $PROJECT_ROOT/build/quickhull.profile > ../build/qprof.pdf
+gnome-open ../build/qprof.pdf
